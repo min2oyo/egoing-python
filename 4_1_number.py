5a8083b8a7cc5be4da05a765441913e15bb5affa
min2oyo <min2u@outlook.com>
2022-05-06 12:06:01 +0900

    4.1 숫자형 데이터 타입

---
+++ 4_1_number.py
@@ -0,0 +1,17 @@
+print(-1)
+print(0)
+print(1) # int
+print(1.1) # float
+print(1+1)
+
+print("1 + 1 =", 1 + 1)
+print("2 - 1 =", 2 - 1)
+print("2 * 2 =", 2 * 2)
+print("4 / 2 =", 4 / 2)
+
+import math
+print("math.sqrt(4):", math.sqrt(4))
+print("math.pow(2, 4):", math.pow(2, 4))
+
+import random
+print("random.random():", random.random())
