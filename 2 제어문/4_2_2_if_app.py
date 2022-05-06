6dc3a87186846dc119ee6074a1d23f35296ec7a4
min2oyo <min2u@outlook.com>
2022-05-06 18:06:03 +0900

    4. 조건문

---
+++ 2 제어문/4_2_2_if_app.py
@@ -0,0 +1,6 @@
+input_id = input("id: ")
+id = "ina"
+if input_id == id:
+    print("Welcome")
+else:
+    print("Who?")
---
+++ 2 제어문/4_3_2_if_app.py
@@ -0,0 +1,9 @@
+input_id = input("id: ")
+id1 = "ina"
+id2 = "ria"
+if input_id == id1:
+    print("Welcome " + id1)
+elif input_id == id2:
+    print("Welcome " + id2)
+else:
+    print("Who?")
